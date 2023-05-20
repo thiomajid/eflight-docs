5810763304406b2404561393eb873f99c8b9a130
ATOM36 <tchiombianomajid@gmail.com>
2023-05-20 04:05:39 +0100

    First commit

---
+++ app.config.ts
@@ -0,0 +1,43 @@
+export default defineAppConfig({
+  docus: {
+    title: "EfLight",
+    titleTemplate: "%s | EfLight",
+    description: "Goodbye to boilerplate using Framework Core.",
+    image: "./public/cover.png",
+    socials: {
+      twitter: "ThioMajid",
+      github: "ATOM36/EfLight",
+    },
+    github: {
+      dir: "EfLight",
+      branch: "main",
+      repo: "EfLight",
+      owner: "ATOM36",
+      edit: true,
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: [],
+    },
+    main: {
+      padded: true,
+      fluid: true,
+    },
+    header: {
+      logo: false,
+      title: "EfLight",
+      showLinkIcon: true,
+      exclude: [],
+      fluid: true,
+    },
+    footer: {
+      credits: {
+        icon: "basil:user-solid",
+        text: "Abdoul Majid Thiombiano\n\n Copyright © 2023 - present MIT license",
+        href: "https://thiomajid.vercel.app",
+      },
+      fluid: true,
+    },
+  },
+});
---
+++ components/content/Link.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href: string;
+  blank?: boolean;
+}>();
+</script>
+
+<template>
+  <a :href="props.href" :target="props.blank ? '_blank' : '_self'" />
+</template>
